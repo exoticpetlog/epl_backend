d7fd813e2b1e364f083b15a1edf91333d66ca9d6
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-12 17:27:22 -0400

    more checks for auth endpoints

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -7,12 +7,32 @@ const authRouter = express.Router();
 
 authRouter.post("/register", async (req, res, next) => {
   try {
+    // check for proper registration info
+    if (
+      !req.body.username ||
+      !req.body.email ||
+      !req.body.password1 ||
+      !req.body.password2
+    ) {
+      res
+        .status(400)
+        .json({ message: "please provide username, passwords, and email" });
+      return;
+    }
+    // check passwords match
+    if (req.body.password1 !== req.body.password2) {
+      res.status(400).json({ message: "passwords do not match" });
+      return;
+    }
+
+    // insert new user to database
     const ids = await db("users").insert({
       username: req.body.username,
-      password: bcrypt.hashSync(req.body.password, 8),
+      password: bcrypt.hashSync(req.body.password1, 8),
       email: req.body.email
     });
 
+    // respond with token
     res.status(201).json({
       message: `Welcome, ${req.body.username}!`,
       token: getToken(ids[0])
@@ -24,19 +44,32 @@ authRouter.post("/register", async (req, res, next) => {
 
 authRouter.post("/login", async (req, res, next) => {
   try {
+    // check body for required login info
+    if (!req.body.username || !req.body.password) {
+      res
+        .status(400)
+        .json({ message: "please provide both username and password" });
+      return;
+    }
+
+    // retrieve user info from database
     const user = await db("users")
       .where({ username: req.body.username })
       .first();
+
     if (user) {
+      // check password and respond with token
       if (bcrypt.compareSync(req.body.password, user.password)) {
         res.status(200).json({
           message: `Welcome Back, ${req.body.username}`,
           token: getToken(user.id)
         });
       } else {
+        // pw did not match
         res.status(400).json({ message: "access denied" });
       }
     } else {
+      // no user by that name in database
       res.status(404).json({ message: "user not found" });
     }
   } catch (err) {
@@ -70,7 +103,7 @@ const verifyToken = async (req, res, next) => {
       }
     } else {
       res.status(400).json({
-        message: "please provide an auth token in headers as 'authorization'"
+        message: "please provide your auth token in headers as 'authorization'"
       });
     }
   } catch (error) {
