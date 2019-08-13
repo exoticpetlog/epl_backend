063e0e864efcc4af752b858c6b7c954b781db204
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-12 23:36:24 -0400

    missing info test

---
+++ api/auth/auth.test.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+const request = require("supertest");
+const db = require("../../config/dbConfig.js");
+const server = require("../server.js");
+
+beforeAll(async () => {
+  await db("users").truncate();
+});
+
+afterEach(async () => {
+  await db("users").truncate();
+});
+
+describe("auth Routes", () => {
+  describe("register endpoint", () => {
+    describe("responds if missing info", () => {
+      const username = "test1";
+      const password1 = "pass1";
+      const password2 = "pass2";
+      const email = "email";
+      test("if missing username", () => {
+        request(server)
+          .post("/auth/register")
+          .send({
+            password1,
+            password2,
+            email
+          })
+          .then(res => {
+            expect(res.status).toBe(400);
+            expect(res.body.message).toBe(
+              "please provide username, passwords, and email"
+            );
+          });
+      });
+      test("if missing password1", () => {
+        request(server)
+          .post("/auth/register")
+          .send({
+            username,
+            password2,
+            email
+          })
+          .then(res => {
+            expect(res.status).toBe(400);
+          });
+      });
+      test("if missing password2", () => {
+        request(server)
+          .post("/auth/register")
+          .send({
+            username,
+            password1,
+            email
+          })
+          .then(res => {
+            expect(res.status).toBe(400);
+          });
+      });
+      test("if missing email", () => {
+        request(server)
+          .post("auth/register")
+          .send({
+            username,
+            password1,
+            password2
+          })
+          .then(res => {
+            expect(res.status).toBe(400);
+          });
+      });
+    });
+  });
+});
