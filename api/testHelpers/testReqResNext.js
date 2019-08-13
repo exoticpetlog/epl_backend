0318830cc97e29ef92f024721a0759b227567675
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-13 19:44:24 -0400

    oops, forgot to delete commented out goo

--- api/testHelpers/testReqResNext.js
+++ api/testHelpers/testReqResNext.js
@@ -19,24 +19,3 @@ module.exports = {
   },
   next: function() {}
 };
-// class Req {
-//   constructor(body) {
-//     this.body = body;
-//   }
-// }
-
-// class Res {
-//   constructor() {
-//     this.statusCode = 0;
-//     this.jsonSent = "";
-//   }
-//   status(statusCode) {
-//     this.statusCode = statusCode;
-//     return this;
-//   }
-//   json(data) {
-//     this.jsonSent = data;
-//   }
-// }
-
-// const next = () => {};
