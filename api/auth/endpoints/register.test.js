0f56c284615e7f77dd5c8d0da352e2a88e710f3d
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-13 20:25:43 -0400

    registration tests complete

--- api/auth/endpoints/register.test.js
+++ api/auth/endpoints/register.test.js
@@ -16,66 +16,6 @@ describe("Auth Routes", () => {
     const password1 = "pass1";
     const password2 = "pass2";
     const email = "email";
-    describe("responds 400 + message if missing info", () => {
-      test("if missing username", () => {
-        const res = new Res();
-        const req = new Req({
-          password1,
-          password2,
-          email
-        });
-        register(req, res, next);
-        expect(res.statusCode).toBe(400);
-        expect(res.jsonSent.message).not.toBeUndefined();
-      });
-      test("if missing password1", () => {
-        const res = new Res();
-        const req = new Req({
-          username,
-          password2,
-          email
-        });
-        register(req, res, next);
-        expect(res.statusCode).toBe(400);
-        expect(res.jsonSent.message).not.toBeUndefined();
-      });
-      test("if missing password2", () => {
-        const res = new Res();
-        const req = new Req({
-          username,
-          password1,
-          email
-        });
-        register(req, res, next);
-        expect(res.statusCode).toBe(400);
-        expect(res.jsonSent.message).not.toBeUndefined();
-      });
-      test("if missing email", () => {
-        const res = new Res();
-        const req = new Req({
-          username,
-          password1,
-          password2
-        });
-        register(req, res, next);
-        expect(res.statusCode).toBe(400);
-        expect(res.jsonSent.message).not.toBeUndefined();
-      });
-    });
-    describe("responds 400 + message if passwords don't match", () => {
-      test("password1 != password2", () => {
-        const res = new Res();
-        const req = new Req({
-          username,
-          password1,
-          password2,
-          email
-        });
-        register(req, res, next);
-        expect(res.statusCode).toBe(400);
-        expect(res.jsonSent.message).not.toBeUndefined();
-      });
-    });
     describe("good info creates user", () => {
       test("responds with 201 + token", async () => {
         const res = new Res();
@@ -87,13 +27,97 @@ describe("Auth Routes", () => {
         });
         await register(req, res, next);
         expect(res.statusCode).toBe(201);
-        expect(res.jsonSent.token).not.toBeUndefined();
+        expect(res.jsonSent.token).toBeTruthy();
       });
       test("adds user to database", async () => {
         const user = await db("users")
           .where({ username })
           .first();
         expect(user).not.toBeUndefined();
+      });
+    });
+    describe("responds 400 + message if:", () => {
+      test("missing username", () => {
+        const res = new Res();
+        const req = new Req({
+          password1,
+          password2,
+          email
+        });
+        register(req, res, next);
+        expect(res.statusCode).toBe(400);
+        expect(res.jsonSent.message).toContain("username");
+      });
+      test("missing password1", () => {
+        const res = new Res();
+        const req = new Req({
+          username,
+          password2,
+          email
+        });
+        register(req, res, next);
+        expect(res.statusCode).toBe(400);
+        expect(res.jsonSent.message).toContain("password1");
+      });
+      test("missing password2", () => {
+        const res = new Res();
+        const req = new Req({
+          username,
+          password1,
+          email
+        });
+        register(req, res, next);
+        expect(res.statusCode).toBe(400);
+        expect(res.jsonSent.message).toContain("password2");
+      });
+      test("missing email", () => {
+        const res = new Res();
+        const req = new Req({
+          username,
+          password1,
+          password2
+        });
+        register(req, res, next);
+        expect(res.statusCode).toBe(400);
+        expect(res.jsonSent.message).toContain("email");
+      });
+      test("password1 != password2", () => {
+        const res = new Res();
+        const req = new Req({
+          username,
+          password1,
+          password2,
+          email
+        });
+        register(req, res, next);
+        expect(res.statusCode).toBe(400);
+        expect(res.jsonSent.message).toContain("not match");
+      });
+      test("username is taken", async () => {
+        const res = new Res();
+        const req = new Req({
+          username,
+          password1,
+          password2: password1,
+          email: "someotheremail"
+        });
+        await register(req, res, next);
+        expect(res.statusCode).toBe(400);
+        expect(res.jsonSent.message).toContain("username");
+        expect(res.jsonSent.message).toContain("in use");
+      });
+      test("email is taken", async () => {
+        const res = new Res();
+        const req = new Req({
+          username: "someotherusername",
+          password1,
+          password2: password1,
+          email
+        });
+        await register(req, res, next);
+        expect(res.statusCode).toBe(400);
+        expect(res.jsonSent.message).toContain("email");
+        expect(res.jsonSent.message).toContain("in use");
       });
     });
   });
