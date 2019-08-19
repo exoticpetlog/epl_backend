a071a11d0729be0b10fa43e5a2b84cd33a45511b
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-19 19:20:11 -0400

    getOrgs tests

--- api/graphql/orgs/orgsResolves.test.js
+++ api/graphql/orgs/orgsResolves.test.js
@@ -8,11 +8,19 @@ describe("GraphQL", () => {
     await db.raw("TRUNCATE TABLE orgs RESTART IDENTITY CASCADE");
     await db.raw("TRUNCATE TABLE users_orgs RESTART IDENTITY CASCADE");
   });
+
   describe("Resolve Functions", () => {
     afterAll(() => {
       db.destroy();
     });
+
     describe("orgs resolves", () => {
+      const req1 = new Req();
+      const req2 = new Req();
+      const args1 = { name: "My Test Org" };
+      const args2 = { name: "Second Org" };
+      let org_id, user1, user2, res;
+
       beforeAll(async () => {
         await db("users").insert([
           {
@@ -27,36 +35,51 @@ describe("GraphQL", () => {
           }
         ]);
       });
+
       describe("createOrg", () => {
-        const req = new Req();
-        const name = "My Test Org";
-        const args = { name };
-        let org_id, user, res;
         test("should put club on db", async () => {
-          user = await db("users")
+          // get IDs for the users
+          user1 = await db("users")
             .where({ username: "orgsTest1" })
             .first();
-          req.user = user;
-          res = await createOrg(null, args, req);
-          const club = await db("orgs")
-            .where({ name })
+          req1.user = user1;
+
+          user2 = await db("users")
+            .where({ username: "orgsTest2" })
             .first();
-          org_id = club.id;
-          expect(club).toBeTruthy();
+          req2.user = user2;
+          // insert their orgs
+          res = await createOrg(null, args1, req1);
+          await createOrg(null, args2, req2);
+
+          const org = await db("orgs")
+            .where({ name: args1.name })
+            .first();
+          org_id = org.id;
+          expect(org).toBeTruthy();
         });
+
         test("should add many-to-many relation", async () => {
           const relation = await db("users_orgs")
             .where({ org_id })
             .first();
-          expect(relation.user_id).toEqual(user.id);
+          expect(relation.user_id).toEqual(user1.id);
         });
+
         test("should return inserted org", async () => {
-          expect(res.name).toEqual(name);
+          expect(res.name).toEqual(args1.name);
         });
       });
+
       describe("getOrgs", () => {
-        test("", () => {
-          expect(true).toBeTruthy();
+        test("should return list of orgs user is in", async () => {
+          const orgsList = await getOrgs(null, args1, req1);
+          expect(orgsList[0].name).toEqual(args1.name);
+        });
+
+        test("should not return orgs of other users", async () => {
+          const orgsList = await getOrgs(null, args1, req1);
+          expect(orgsList[1]).toBeFalsy();
         });
       });
     });
