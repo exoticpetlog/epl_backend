30f376499115c8841ad1c66fab42cd6534315031
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-19 17:00:44 -0400

    added beforeAll insert for 2 users

--- api/graphql/orgs/orgsResolves.test.js
+++ api/graphql/orgs/orgsResolves.test.js
@@ -1,18 +1,36 @@
+const pause = require("../../testHelpers/pauseTest.js");
+const db = require("../../../config/dbConfig.js");
 const { getOrgs, createOrg, updateOrg } = require("./orgsResolves");
 
-// beforeAll(async () => {
-//   return db.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
-// });
+beforeAll(async () => {
+  await db.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
+  return pause();
+});
 
 describe("GraphQL", () => {
   describe("Resolve Functions", () => {
     beforeAll(async () => {
-      await new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, 200);
+      await db("users").insert([
+        {
+          username: "orgsTest1",
+          password: "password1",
+          email: "email1@2.test"
+        },
+        {
+          username: "orgsTest2",
+          password: "password2",
+          email: "email2@2.test"
+        }
+      ]);
+
+      return pause();
+    });
+    describe("orgs resolves", () => {
+      describe("getOrgs", () => {
+        test("", () => {
+          expect(true).toBeTruthy();
+        });
       });
     });
-    describe("orgs resolves", () => {});
   });
 });
