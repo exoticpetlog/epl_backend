3aa36936ca2a461b46d70ccfff4d6d7e739b9fd7
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-27 13:49:49 -0400

    extracted access helper function

--- api/graphql/animals/animalsResolves.js
+++ api/graphql/animals/animalsResolves.js
@@ -1,17 +1,17 @@
 const db = require("../../../config/dbConfig.js");
-const { GraphQLError } = require("graphql");
+const { checkAccess } = require("../authorization/accessHelpers.js");
 
-async function checkAccess(args, req) {
-  const hasAccess = await db("users_orgs")
-    .where({
-      org_id: args.org_id,
-      user_id: req.user.id
-    })
-    .first();
-  if (!hasAccess) {
-    throw new GraphQLError(`You do not have access to org: ${args.org_id}`);
-  }
-}
+// async function checkAccess(args, req) {
+//   const hasAccess = await db("users_orgs")
+//     .where({
+//       org_id: args.org_id,
+//       user_id: req.user.id
+//     })
+//     .first();
+//   if (!hasAccess) {
+//     throw new GraphQLError(`You do not have access to org: ${args.org_id}`);
+//   }
+// }
 
 module.exports = {
   getAnimals: async (parentValue, args, req) => {
@@ -27,7 +27,23 @@ module.exports = {
     return animal;
   },
 
-  createAnimal: async (parentValue, args, req) => {},
+  createAnimal: async (parentValue, args, req) => {
+    await checkAccess(args, req);
+    const [animal] = await db("animals")
+      .insert(args)
+      .returning("*");
+    return animal;
+  },
 
-  updateAnimal: async (parentValue, args, req) => {}
+  updateAnimal: async (parentValue, args, req) => {
+    const animal = await db("animals")
+      .where({ id: args.id })
+      .first();
+    await checkAccess(animal, req);
+    const [updated] = await db("animals")
+      .where({ id: args.id })
+      .update(args)
+      .returning("*");
+    return updated;
+  }
 };
--- api/graphql/species/speciesResolves.js
+++ api/graphql/species/speciesResolves.js
@@ -1,17 +1,5 @@
 const db = require("../../../config/dbConfig.js");
-const { GraphQLError } = require("graphql");
-
-async function checkAccess(args, req) {
-  const hasAccess = await db("users_orgs")
-    .where({
-      org_id: args.org_id,
-      user_id: req.user.id
-    })
-    .first();
-  if (!hasAccess) {
-    throw new GraphQLError(`You do not have access to org: ${args.org_id}`);
-  }
-}
+const { checkAccess } = require("../authorization/accessHelpers.js");
 
 module.exports = {
   getSpecies: async (parentValue, args, req) => {
