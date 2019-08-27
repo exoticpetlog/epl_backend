3f86754ba3923f642337957564861660d203615d
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-27 18:49:09 -0400

    itemsSchema written

---
+++ api/graphql/fields/items/itemsResolves.js
@@ -0,0 +1,10 @@
+const db = require("../../../../config/dbConfig.js");
+const { checkAccess } = require("../../authorization/accessHelpers.js");
+
+module.exports = {
+  getItems: async (parentValue, args, req) => {},
+
+  createItem: async (parentValue, args, req) => {},
+
+  updateItem: async (parentValue, args, req) => {}
+};
