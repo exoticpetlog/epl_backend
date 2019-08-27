8e721171b0dd82004443662a0434d18e1ec0df4d
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-27 15:20:50 -0400

    actionsResolves complete

--- api/graphql/fields/actions/actionsResolves.js
+++ api/graphql/fields/actions/actionsResolves.js
@@ -7,7 +7,7 @@ module.exports = {
       .where({ id: args.species_id })
       .first();
     await checkAccess(species, req);
-    return await db("actions").where(args);
+    return await db("actions").where({ species_id: args.species_id });
   },
   createAction: async (parentValue, args, req) => {
     const species = await db("species")
@@ -19,5 +19,15 @@ module.exports = {
       .returning("*");
     return inserted;
   },
-  updateAction: async (parentValue, args, req) => {}
+  updateAction: async (parentValue, args, req) => {
+    const species = await db("actions")
+      .where({ id: args.id })
+      .join("species", "species.id", "=", "actions.species_id");
+    await checkAccess(species, req);
+    const [updated] = await db("actions")
+      .where({ id: args.id })
+      .update(args)
+      .returning("*");
+    return updated;
+  }
 };
