4ea0140987953dbff330b601e78b1a20cfbbeac3
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-27 16:05:44 -0400

    historyType defined

--- api/graphql/fields/history/historySchema.js
+++ api/graphql/fields/history/historySchema.js
@@ -13,7 +13,19 @@ const {
   updateHistory
 } = require("./historyResolves.js");
 
-const historyType = new GraphQLObjectType({});
+const historyType = new GraphQLObjectType({
+  name: "history",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    animal_id: { type: GraphQLInt },
+    action_id: { type: GraphQLInt },
+    success: { type: GraphQLBoolean },
+    is_complete: { type: GraphQLBoolean },
+    initiating_user: { type: GraphQLInt },
+    closing_user: { type: GraphQLInt },
+    created_at: { type: GraphQLString }
+  })
+});
 
 module.exports = {
   historyQueryFields: {},
