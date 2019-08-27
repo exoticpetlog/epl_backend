9a2b12135dd73ef9d5c861a9d448e43b5cddaa1f
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-27 18:18:41 -0400

    got rid of history_items table, put quantities in history and reminders

--- data/migrations/20190823213930_history.js
+++ data/migrations/20190823213930_history.js
@@ -34,6 +34,14 @@ exports.up = function(knex) {
       .onUpdate("CASCADE")
       .onDelete("SET NULL");
     // ^^ force index ?  or not needed? .. leave out for now...
+    table.integer("item_id").unsigned();
+    table
+      .foreign("item_id")
+      .references("items.id")
+      .onUpdate("CASCADE")
+      .onDelete("SET NULL");
+    // ^^ force index ?  or not needed? .. leave out for now...
+    table.integer("quantity").unsigned();
     table.timestamps(true, true);
   });
 };
