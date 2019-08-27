ff3050ac11a00de9d927d1795d9d9d606222fd5a
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-27 15:11:58 -0400

    some resolves written

--- api/graphql/fields/actions/actionsResolves.js
+++ api/graphql/fields/actions/actionsResolves.js
@@ -2,7 +2,22 @@ const db = require("../../../../config/dbConfig.js");
 const { checkAccess } = require("../../authorization/accessHelpers.js");
 
 module.exports = {
-  getActions: async (parentValue, args, req) => {},
-  createAction: async (parentValue, args, req) => {},
+  getActions: async (parentValue, args, req) => {
+    const species = await db("species")
+      .where({ id: args.species_id })
+      .first();
+    await checkAccess(species, req);
+    return await db("actions").where(args);
+  },
+  createAction: async (parentValue, args, req) => {
+    const species = await db("species")
+      .where({ id: args.species_id })
+      .first();
+    await checkAccess(species, req);
+    const [inserted] = await db("actions")
+      .insert(args)
+      .returning("*");
+    return inserted;
+  },
   updateAction: async (parentValue, args, req) => {}
 };
