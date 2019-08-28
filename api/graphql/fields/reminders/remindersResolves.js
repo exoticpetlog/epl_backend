b8ed88cb4e01bd017065094622a43822629444a4
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-27 21:54:31 -0400

    added first two resolves

--- api/graphql/fields/reminders/remindersResolves.js
+++ api/graphql/fields/reminders/remindersResolves.js
@@ -2,9 +2,25 @@ const db = require("../../../../config/dbConfig.js");
 const { checkAccess } = require("../../authorization/accessHelpers.js");
 
 module.exports = {
-  getReminders: async (parentValue, args, req) => {},
+  getReminders: async (parentValue, args, req) => {
+    await checkAccess(args, req);
+    return await db("reminders").where({ org_id: args.org_id });
+  },
 
-  createReminder: async (parentValue, args, req) => {},
+  createReminder: async (parentValue, args, req) => {
+    await checkAccess(args, req);
+    if (!args.next_due) {
+      let now = new Date();
+      args.next_due = now;
+      if (args.frequency) {
+        args.next_due = now.setDate(now.getDate() + frequency);
+      }
+    }
+    const [inserted] = await db("reminders")
+      .insert(args)
+      .returning("*");
+    return inserted;
+  },
 
   updateReminder: async (parentValue, args, req) => {}
 };
