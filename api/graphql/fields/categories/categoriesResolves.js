b0fa8c894632c03ad5d69a1ba624da75c7acff8c
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-29 13:35:33 -0400

    categories resolves started

--- api/graphql/fields/categories/categoriesResolves.js
+++ api/graphql/fields/categories/categoriesResolves.js
@@ -2,9 +2,21 @@ const db = require("../../../../config/dbConfig.js");
 const { checkAccess } = require("../../authorization/accessHelpers.js");
 
 module.exports = {
-  getCategories: async (parentValue, args, req) => {},
+  getCategories: async (parentValue, args, req) => {
+    await checkAccess(args, req);
+    return await db("categories").where({ org_id: args.org_id });
+  },
 
-  createCategory: async (parentValue, args, req) => {},
+  createCategory: async (parentValue, args, req) => {
+    await checkAccess(args, req);
+    const [inserted] = await db("categories")
+      .insert({
+        user_id: args.user_id,
+        name: args.name,
+      })
+      .returning("*");
+    return inserted;
+  },
 
   updateCategory: async (parentValue, args, req) => {},
 };
