5eff4520954a07142ac86588a92484e636ec836c
Addison-Stavlo <AddieStavlo@gmail.com>
2019-09-13 12:53:51 -0400

    tags schema started

---
+++ api/graphql/fields/categories/tags/tagsSchema.js
@@ -0,0 +1,44 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+} = require("graphql");
+
+const { getByTag, addTag, removeTag } = require("./membersResolves");
+const { animalsType } = require("../../animals/animalsSchema");
+
+const tagsQueryFields = {
+  animalsByTag: {
+    type: new GraphQLList(animalsType),
+    args: {
+      category_id: { type: new GraphQLNonNull(GraphQLInt) },
+    },
+    resolve: getByTag,
+  },
+};
+
+const tagsMutationFields = {
+  addTag: {
+    type: animalsType,
+    args: {
+      animal_id: { type: new GraphQLNonNull(GraphQLInt) },
+      tag_id: { type: new GraphQLNonNull(GraphQLInt) },
+    },
+    resolve: addTag,
+  },
+  removeTag: {
+    type: animalsType,
+    args: {
+      animal_id: { type: new GraphQLNonNull(GraphQLInt) },
+      tag_id: { type: new GraphQLNonNull(GraphQLInt) },
+    },
+    resolve: removeTag,
+  },
+};
+
+module.exports = {
+  tagsQueryFields,
+  tagsMutationFields,
+};
